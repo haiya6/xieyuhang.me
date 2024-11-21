bd175d55f49bde30ff8546af0bb6765a2d9394c0
xieyuhang <13451213869@163.com>
2024-11-21 11:43:32 +0800

    chore: update

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,7 +4,51 @@ import PostList from './PostList.vue'
 
 <template>
   <Post>
-    <h2>一些文章</h2>
+    <h1>你好</h1>
+    <p>谢宇航，是一名前端开发工程师，源码熟悉前端框架 Vue、React，爱好和擅长前端基建开发</p>
+    <p>我的开源项目：</p>
+    <ul>
+      <li>
+        <a href="https://github.com/xieyhn/weapp-scroll" target="_blank">
+          weapp-scroll，是一个微信小程序滚动插件，利用 WXS 编写，为统一 iOS 和 Android 的滚动体验
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/xieyhn/vue-presetup" target="_blank">
+          vue-presetup，基于 Vue3，将组件进行预加载，便于优化页面切换体验
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/xieyhn/vite-plugin-html-resolve-alias" target="_blank">
+          vite-plugin-html-resolve-alias，是一个 Vite 插件，可以在 HTML 中使用 alias 配置
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/xieyhn/vite-plugin-legacy-dev" target="_blank">
+          vite-plugin-legacy-dev，是一个 Vite 插件，用于开发环境兼容旧版本浏览器
+        </a>
+      </li>
+    </ul>
+    <p>另外擅长特效、响应式开发，这里有一些案例：</p>
+    <blockquote>
+      <p>均能访问，但可能需要等待加载</p>
+    </blockquote>
+    <ul>
+      <li>
+        <a href="https://works.xieyuhang.me/wenjinshuyuan/" target="_blank">湖北省重点景区问津书院官方网站</a>
+      </li>
+      <li>
+        <a href="https://works.xieyuhang.me/yzy/" target="_blank">武汉友芝友医疗科技官方网站</a>
+      </li>
+      <li>
+        <a href="https://works.xieyuhang.me/screen-a/" target="_blank">可视化大屏 A</a>
+      </li>
+      <li>
+        <a href="https://works.xieyuhang.me/screen-b/" target="_blank">可视化大屏 B</a>
+      </li>
+    </ul>
+    <p>更多开源贡献访问 <a href="https://github.com/xieyhn" target="_blank">我的 GitHub</a></p>
+    <h2>文章</h2>
     <PostList />
   </Post>
 </template>
